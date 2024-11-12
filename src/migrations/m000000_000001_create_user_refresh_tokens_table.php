b372f15ed2f1a75628f142e526578aa70fe36872
Oussama <github@yawaweb.com>
2024-11-13 00:38:06 +0100

    Update migration.

--- src/migrations/m000000_000001_create_user_refresh_tokens_table.php
+++ src/migrations/m000000_000001_create_user_refresh_tokens_table.php
@@ -12,20 +12,23 @@ class m000000_000001_create_user_refresh_tokens_table extends Migration
     public function safeUp()
     {
         $this->createTable('{{%user_refresh_tokens}}', [
-            'user_refresh_tokenID' => $this->primaryKey()->unsigned()->notNull(),
-            'urf_userID' => $this->integer(10)->unsigned()->notNull(),
-            'urf_token' => $this->string(1000)->notNull(),
-            'urf_ip' => $this->string(50)->notNull(),
-            'urf_user_agent' => $this->string(1000)->notNull(),
-            'urf_created' => $this->dateTime()->notNull()->comment('UTC'),
+            'id' => $this->primaryKey()->unsigned()->notNull(),
+            'user_id' => $this->integer(10)->unsigned()->notNull(),
+            'token' => $this->string(1000)->notNull(),
+            'ip' => $this->string(50)->notNull(),
+            'user_agent' => $this->string(1000)->notNull(),
+            'created_at' => $this->dateTime()->notNull()->comment('UTC'),
         ], 'COMMENT="For JWT authentication process"');
+
+        $this->addForeignKey('fk-user_refresh_tokens-user_id', '{{%user_refresh_tokens}}', 'user_id', '{{%user}}', 'id', 'CASCADE', 'CASCADE');
     }
 
-    /**
-     * {@inheritdoc}
-     */
     public function safeDown()
     {
+        // Drop the foreign key constraint first
+        $this->dropForeignKey('fk-user_refresh_tokens-user_id', '{{%user_refresh_tokens}}');
+
+        // Then drop the table
         $this->dropTable('{{%user_refresh_tokens}}');
     }
 }
